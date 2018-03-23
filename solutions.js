1e7ba28d404a40dd700bcccb3b23e5e645163413
Jason Spiller <jason@thetargetlab.com>
2018-03-22 20:47:40 -0600

    Commit 2 - Palindrome

--- solutions.js
+++ solutions.js
@@ -4,4 +4,9 @@
 // 3. Within a function, what is the difference between return and console.log?
 // return is the value that is return from a function call. You can also return the same function to create a recursive function. Console.log(); is a method that outputs to the console specifically.
 
- 		
+
+// Palindrome
+function isPalindrome(word) {
+	return word.toUpperCase() === word.split('').reverse().join('').toUpperCase();
+}
+console.log('Is palindrome? ' + isPalindrome('test'));
